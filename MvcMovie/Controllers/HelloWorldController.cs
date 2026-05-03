0a5fd96a4b3ea0cfed1960f97777b52469b54bde
ST10473924 <ST10473924@vcconnect.edu.za>
2026-05-03 16:23:02 +0200

    Pushing finished tutorial
    Completed Steps 1 - 9 for MvcMovies from Microsoft learn site:
    https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-10.0&tabs=visual-studio

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // Get: /HellowWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //// 
+        //// GET: /HelloWorld/Welcome/ 
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+        //// Get: /HelloWorld/Welcome/
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+
+        //}
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
